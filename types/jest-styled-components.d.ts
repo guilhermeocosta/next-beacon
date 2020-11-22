de8cd2c3e9a209ca83c27189efd2217d7c557e79
Guilherme de Oliveira Costa <guilhermedeocosta@gmail.com>
2020-11-22 00:45:10 -0300

    fix: Jest Styled Components type support
    - Add support for jest-styled-components types;
    
    - Move type declaration files to new types folder.

---
+++ types/jest-styled-components.d.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/ban-types */
+/**
+ * @see https://github.com/styled-components/jest-styled-components/pull/308
+ */
+import { Plugin, NewPlugin } from 'pretty-format';
+
+declare global {
+  namespace jest {
+    interface AsymmetricMatcher {
+      $$typeof: Symbol;
+      sample?: string | RegExp | object | Array<any> | Function;
+    }
+
+    type Value = string | number | RegExp | AsymmetricMatcher | undefined;
+
+    interface Options {
+      media?: string;
+      modifier?: string;
+      supports?: string;
+    }
+
+    interface Matchers<R, T> {
+      toHaveStyleRule(property: string, value?: Value, options?: Options): R;
+    }
+  }
+}
+
+export declare const styleSheetSerializer: Exclude<Plugin, NewPlugin>;
