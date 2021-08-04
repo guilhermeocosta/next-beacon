ab7a516176ce97fe36b29079f7b30a3b263f9934
Guilherme de Oliveira Costa <guilhermedeocosta@gmail.com>
2021-08-04 17:24:22 -0300

    fix: Storybookss build
    - Fix storybook build config file to enable static serving.

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,13 @@ module.exports = {
     '../src/**/stories.@(js|jsx|ts|tsx)',
   ],
   addons: ['@storybook/addon-essentials'],
+  babel: async (options) => ({
+    ...options,
+    plugins: [
+      ...options.plugins,
+      require.resolve('@babel/plugin-transform-react-jsx')
+    ]
+  }),
   webpackFinal: (config) => {
     config.resolve.modules.push(`${process.cwd()}/src`);
     return config;
